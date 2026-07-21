759568830393ba66baa54c124e4cbc8cd7b170a2
simooooes1101 <edcarlos.scamargo@gmail.com>
2026-07-21 02:11:29 -0300

    fix: substitui chave anon pelo token JWT valido eyJ

--- env.js
+++ env.js
@@ -1,4 +1,4 @@
 window.ENV = {
     SUPABASE_URL: 'https://ruytftiztkrkvniqqmjj.supabase.co',
-    SUPABASE_KEY: 'sb_publishable_70qktfjIX0DcfY2O-YM3Fw_FZbjUkEc'
+    SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ1eXRmdGl6dGtya3ZuaXFxbWpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk5MzAwMzgsImV4cCI6MjA5NTUwNjAzOH0.anrNIg32wkHtv00qIZMMowqiXFRWQcLJpxxtOxOyP3M'
 };
